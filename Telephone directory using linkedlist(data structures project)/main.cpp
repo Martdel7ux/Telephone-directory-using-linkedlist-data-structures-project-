c6b0a04632d25f727bdc60af2de67d849e614763
Martin Ahoto <martinahoto4@gmail.com>
2022-05-12 00:11:31 +0300

    Initial Commit

---
+++ Telephone directory using linkedlist(data structures project)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Telephone directory using linkedlist(data structures project)
+//
+//  Created by MARTIN on 12/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
